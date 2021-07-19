5b2a1f8b0b707159194a51152eb629c65f8e2640
megandls <megandeleon5@gmail.com>
2021-07-19 16:26:44 -0600

    directory

--- js/directory.js
+++ js/directory.js
@@ -1,34 +1,33 @@
 const local = 'json/names.json';
 fetch(local)
-  .then(function (response) {
+.then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    const busi = jsonObject['business'];
+    const busi = jsonObject['business']; 
     for (let i = 0; i < busi.length; i++)
   {
         let details = document.createElement('section');
-        details.setAttribute('class', 'item');
+         
+        details.setAttribute('class', 'item' + i);
 
-        let h2 = document.createElement('h2');
-        let h3 = document.createElement('h3');
-        let p4 = document.createElement('p');
-        let image = document.createElement('img');      
+         let h2 = document.createElement('h2');
+         let h3 = document.createElement('p');
+         let p4 = document.createElement('p');
+         let image = document.createElement('img');      
 
-        h2.textContent = busi[i].name;
-        h3.textContent = busi[i].website;
-        p4.textContent = "Phone Number: " + busi[i].number;
+         h2.textContent = busi[i].name;
+         h3.textContent = "* Website: " + busi[i].website;
+         p4.textContent = "* Phone Number: " + busi[i].number;
         
-        image.setAttribute('src', "images/" + busi[i].logo);
-        image.setAttribute('alt', "photo of " + busi[i].name);
-        image.setAttribute('class', "itemimg");
+         image.setAttribute('src', "images/" + busi[i].logo);
+         image.setAttribute('alt', "photo of " + busi[i].name);
 
-        details.appendChild(h2);
-        details.appendChild(h3);
-        details.appendChild(p4);
+         details.appendChild(h2);
+         details.appendChild(h3);
+         details.appendChild(p4);
         details.appendChild(image);
 
-        document.querySelector('div.data').appendChild(details);
-    }
-  }
-);
+       document.querySelector('div.data').appendChild(details);
+   }
+  });
--- js/gridlist.js
+++ js/gridlist.js
@@ -1,17 +1,27 @@
 function list() {
-document.getElementById('column1').style.width = '100%';
-document.getElementById('column2').style.width = '100%';
-document.getElementById('column3').style.width = '100%';
+document.querySelector('section.item0').style.width = '100%';
+document.querySelector('section.item1').style.width = '100%';
+document.querySelector('section.item2').style.width = '100%';
+document.querySelector('section.item3').style.width = '100%';
+document.querySelector('section.item4').style.width = '100%';
+document.querySelector('section.item5').style.width = '100%';
+document.querySelector('section.item6').style.width = '100%';
+document.querySelector('section.item7').style.width = '100%';
+document.querySelector('section.item8').style.width = '100%';
 }
 
 function medium() {
-    document.getElementById('column1').style.width = '50%';
-    document.getElementById('column2').style.width = '50%';
-    document.getElementById('column3').style.width = '50%';
+    document.querySelector('section.item0').style.width = '50%';
 }
 
 function grid() {
-    document.getElementById('column1').style.width = '25%';
-    document.getElementById('column2').style.width = '25%';
-    document.getElementById('column3').style.width = '25%';
+document.querySelector('section.item0').style.width = '32%';
+document.querySelector('section.item1').style.width = '32%';
+document.querySelector('section.item2').style.width = '32%';
+document.querySelector('section.item3').style.width = '32%';
+document.querySelector('section.item4').style.width = '32%';
+document.querySelector('section.item5').style.width = '32%';
+document.querySelector('section.item6').style.width = '32%';
+document.querySelector('section.item7').style.width = '32%';
+document.querySelector('section.item8').style.width = '32%';
 }
