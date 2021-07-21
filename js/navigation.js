7278537d06b4553380be079cd13bb8493f6fa2d3
megandls <megandeleon5@gmail.com>
2021-07-20 18:58:53 -0600

    clean city

--- js/navigation.js
+++ js/navigation.js
@@ -9,21 +9,4 @@ function closeNav() {
   document.getElementById("scoot").style.marginLeft= "0";
 }
 
-// document.getElementById("myBtn").addEventListener("click", open_close);
 
-// var menuState = 0 // close
-// function open_close() {
-//   if(menuState === 0){
-//      menuState = 1;
-//      document.getElementById("mySidenav").style.width = "250px";
-//      //document.getElementById("main").style.marginLeft = "250px";
-//      document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-//   }
-//   else {
-//      menuState = 0;
-//      document.getElementById("mySidenav").style.width = "0";
-//      //document.getElementById("main").style.marginLeft = "0";
-//      document.body.style.backgroundColor = "white";
-//   }
-//   console.log(menuState);
-// } 
